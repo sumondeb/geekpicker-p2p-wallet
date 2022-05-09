12d19d318fe3a4fa201f33c95e092d18ce5c2dd6
sumondeb <sumondeb5@gmail.com>
2022-05-09 23:21:32 +0600

    Test Done

--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -9,9 +9,9 @@ class UrlTest extends TestCase
 {
     public function test_all_urls_without_auth_returns_successful_response(): void
     {
-        $this->get('/')->assertStatus(302); //Redirect
-        $this->get('/most-conversion')->assertStatus(200);
-        $this->get('/user-transaction-info')->assertStatus(200);
-        $this->get('/transaction-history')->assertStatus(200);
+        $this->get('/')->assertRedirect('most-conversion'); //Redirect
+        $this->get('/most-conversion')->assertOk();
+        $this->get('/user-transaction-info')->assertOk();
+        $this->get('/transaction-history')->assertOk();
     }
 }
